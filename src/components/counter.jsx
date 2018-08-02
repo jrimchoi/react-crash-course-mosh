6e94a51ea245d6ce37b472788337b56763e13055
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-08-02 16:16:44 -0300

    react fragment

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
