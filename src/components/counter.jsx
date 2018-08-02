245e72a3bc747ec360a6826dd8db83974f3010b9
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-08-02 16:30:32 -0300

    styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,18 +5,25 @@ class Counter extends Component {
     count: 0
   };
 
+  styles = {
+    fontWeight: "bold",
+    fontSize: 50
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
