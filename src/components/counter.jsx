0eb6e9efe1ecfcfb7e2b9c27a4f54c09589a1e7a
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-08-04 18:32:57 -0300

    final tutorial

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // AJAX call and get new data from server
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("COunter unmounted");
+  }
+
   render() {
     console.log("counter rendered");
     return (
