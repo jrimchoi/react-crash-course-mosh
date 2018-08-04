9662028adfca5f0e4be717d8a457507754bd7858
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-08-04 17:07:18 -0300

    deleting counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,8 +17,7 @@ class Counter extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <h5>{this.props.id}</h5>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
@@ -26,8 +25,13 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {/* <div>{this.renderTags()}</div> */}
-      </React.Fragment>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,13 +10,22 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
-            <h5>Counter #{counter.id}</h5>
-          </Counter>
+          <Counter
+            key={counter.id}
+            id={counter.id}
+            value={counter.value}
+            onDelete={this.handleDelete}
+          />
         ))}
       </div>
     );
